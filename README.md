4d401d9b149c7c30bc87ec529bdf4a1fdbb9b908
Aly Makhlouf <ahmedmak012.2005@gmail.com>
2025-02-19 01:13:49 +0200

    Remove all files

--- README.md
+++
@@ -1,1 +0,0 @@
-# etabema-backend
--- Utils/ValidationError.js
+++
@@ -1,7 +0,0 @@
-export function ValidationError(error , res){
-    const errors = Object.values(error.errors).map((e)=>({
-        field : e.path,
-        message : e.message
-    }))
-    return res.status(400).json({errors})
-}
